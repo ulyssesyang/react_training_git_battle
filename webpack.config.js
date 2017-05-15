5526a3089d055718147a7b640d1ccc7ddefa58ff
Yu Yang <yu.yang@cittacorp.com>
2017-05-15 17:06:52 -0400

    add component

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,13 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-    context: path.resolve(__dirname, 'app'),
     entry: './app/index.js',
     module: {
-        rules: [{
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'app'),
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        ['es2015', { modules: false }]
-                    ]
-                }
-            }]
-        }]
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
