fa79fb8735844e311052efaeec77bf98e66be32f
Yu Yang <yu.yang@cittacorp.com>
2017-05-20 22:07:26 -0400

    test map and filter func

--- app/index.js
+++ app/index.js
@@ -2,22 +2,50 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-// state
-// lifecycle event
-// UI
-class App extends React.Component {
+var listName = [
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ];
 
-    render() {
-        return (
-            <div>
-                Hello World!
-            </div>
-        )
-    }
-
+class Badge extends React.Component {
+  render() {
+      var friendList = this.props.filter(function (friend) {
+          return friend.friend === true;
+      });
+      var nonfriendList = this.props.filter(function (nonfriend) {
+          return nonfriend.friend === false;
+      });
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {
+              friendList.map(function (user,index) {
+                  return <li key = {user.name}>{user.name}</li>
+              })
+          }
+        </ul>
+        
+        <hr />
+        
+        <h1> Non Friends </h1>
+        <ul>
+          {
+              nonfriendList.map(function (user,index) {
+                  return <li key = {user.name}>{user.name}</li>
+              })
+          }
+        </ul>        
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+  <Users list= {listName}
+  />,
+  document.getElementById('app')
 );
