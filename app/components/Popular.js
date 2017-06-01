f6552ac82ddc16dbe8e47d8f1916d1bf2174bc75
Yu Yang <yu.yang@cittacorp.com>
2017-06-01 16:30:12 -0400

    build up app and popular components and also make basic styling

---
+++ app/components/Popular.js
@@ -0,0 +1,26 @@
+var React = require('react');
+
+class Popular extends React.Component {
+    render() {
+        var languages = [
+            'All',
+            'JavaScript',
+            'Ruby',
+            'Java',
+            'CSS',
+            'Python'
+        ];
+        return (
+            <ul className='languages'>
+                {languages
+                    .map(function (lang) {
+                        return (
+                            <li>{lang}</li>
+                        )
+                    })}
+            </ul>
+        )
+    }
+}
+
+module.exports = Popular
--- app/index.js
+++ app/index.js
@@ -1,60 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
 
 require('./index.css');
 
-var listName = [
-    { name: 'Tyler', friend: true },
-    { name: 'Ryan', friend: true },
-    { name: 'Michael', friend: false },
-    { name: 'Mikenzi', friend: false },
-    { name: 'Jessica', friend: true },
-    { name: 'Dan', friend: false } ];
-
-class Users extends React.Component {
-  render() {
-      var friendList = this.props.list.filter(function (friend) {
-          return friend.friend === true;
-      });
-      var nonfriendList = this.props.list.filter(function (nonfriend) {
-          return nonfriend.friend === false;
-      });
-    return (
-      <div>
-        <h1>Friends</h1>
-        <ul>
-          {
-              friendList.map(function (user,index) {
-                  return <li key = {user.name}>{user.name}</li>
-              })
-          }
-        </ul>
-        
-        <hr />
-        
-        <h1> Non Friends </h1>
-        <ul>
-          {
-              nonfriendList.map(function (user,index) {
-                  return <li key = {user.name}>{user.name}</li>
-              })
-          }
-        </ul>        
-      </div>
-    )
-  }
-}
-
-Users.propTypes = {
-    list: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        friend: PropTypes.bool.isRequired
-    }))
-}
+var App = require('./components/App');
 
 ReactDOM.render(
-  <Users list= {listName}
-  />,
-  document.getElementById('app')
-);
+    <App/>, document.getElementById('app'));
