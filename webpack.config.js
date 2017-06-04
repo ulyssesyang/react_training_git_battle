a05eeaa65c62e084e85261f0f0c2a09da1d5b10d
Yu Yang <ulyssesyangyu@gmail.com>
2017-06-04 18:38:24 -0400

    setup webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 const config = {
     entry: './app/index.js',
@@ -23,6 +24,16 @@ const config = {
         historyApiFallback: true
     },
     plugins: [new HtmlWebpackPlugin({template: 'app/index.html'})]
+}
+
+if (process.env.NODE_ENV === 'production') {
+    config
+        .plugins
+        .push(new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }), new webpack.optimize.UglifyJsPlugin())
 }
 
 module.exports = config
